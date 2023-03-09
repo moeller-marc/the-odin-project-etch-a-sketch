9a3f9dbf16ef8e9771d0b200dc541b9e68e148cf
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2023-03-09 18:28:48 +0100

    add function to change divs to navy blue on hover

--- js/scrypt.js
+++ js/scrypt.js
@@ -5,4 +5,9 @@ for (let num = 0; num < 256; num++) {
   const div = document.createElement("div");
   div.classList.add("subDivs");
   mainDiv.appendChild(div);
+
+  div.addEventListener("mouseenter", () => {
+    console.log("Mouse entered subDiv", num);
+    div.style.backgroundColor = "#000080";
+  });
 }
