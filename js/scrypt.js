91b839296757e3097a9abe84fb2d861b23337128
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2023-03-14 20:37:07 +0100

    rewrite and implement function to draw grid and function to clear grid

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,13 +1,43 @@
 const mainDiv = document.querySelector("#mainDiv");
+const accessories = document.querySelector("#accessories");
 
-for (let num = 0; num < 256; num++) {
-  const element = (256)[num];
-  const div = document.createElement("div");
-  div.classList.add("subDivs");
-  mainDiv.appendChild(div);
-
-  div.addEventListener("mouseenter", () => {
-    console.log("Mouse entered subDiv", num);
-    div.style.backgroundColor = "#000080";
-  });
+function clearGrid() {
+  mainDiv.innerHTML = "";
 }
+
+function drawGrid(x, y) {
+  for (let num = 0; num < y; num++) {
+    for (let xnum = 0; xnum < x; xnum++) {
+      const div = document.createElement("div");
+      div.classList.add("subDivs");
+      div.style.flex = `0 0 ${100 / x}%`;
+      div.style.backgroundColor = "white";
+
+      mainDiv.appendChild(div);
+
+      div.addEventListener("mouseenter", () => {
+        console.log("Mouse entered subDiv", num);
+        div.style.backgroundColor = "#000080";
+      });
+    }
+  }
+}
+const sizeButton = document.createElement("BUTTON");
+sizeButton.textContent = "new grid";
+sizeButton.style.fontSize = "32px";
+accessories.appendChild(sizeButton);
+
+sizeButton.addEventListener("click", () => {
+  clearGrid();
+  let xValue = prompt("x: ", "16");
+  let yValue = prompt("y: ", "16");
+  if (xValue <= 100 && yValue <= 100) {
+  } else {
+    xValue = 16;
+    yValue = 16;
+    alert("no value bigger than 100");
+  }
+  drawGrid(xValue, yValue);
+});
+
+drawGrid(16, 16);
